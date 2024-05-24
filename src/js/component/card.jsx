9e768583693fa1479ef6866cbafc8872c542b22e
Alvaroorta1 <165050201+Alvaroorta1@users.noreply.github.com>
2024-05-24 01:54:51 +0000

    cuarto commit

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -5,8 +5,8 @@ export const Card = () => {
   <img src="https://static.nationalgeographic.es/files/styles/image_3200/public/75552.ngsversion.1422285553360.jpg?w=1900&h=1267" className="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
+    <p className="card-text">"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."</p>
+    <a href="#" className="btn btn-primary">Find Out More!</a>
   </div>
 </div>
 )
