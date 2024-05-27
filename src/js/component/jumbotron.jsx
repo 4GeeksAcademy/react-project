388cfa33600a5b1119587d8287af1578c51599ed
Alvaroorta1 <165050201+Alvaroorta1@users.noreply.github.com>
2024-05-27 20:55:11 +0000

    final commit

--- src/js/component/jumbotron.jsx
+++ src/js/component/jumbotron.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 export const Jumbotron = () => {
     return(
-    <div className="jumbotron p-5 bg-secondary">
+    <div className="jumbotron p-5 bg-light">
   <h1 className="display-4"> A Warm Welcome!</h1>
   <p className="lead">"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."</p>
   <a className="btn btn-primary btn-lg" href="#" role="button">Call to action!</a>
--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
 export const Navbar = () => { 
-    return ( <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
+    return ( <nav className="navbar sticky-top navbar-expand-lg bg-dark navbar-dark">
     <div className="container-fluid">
       <a className="navbar-brand" href="#">Start Bootstrap</a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div style= {{marginLeft:"900px"}} className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
           <li className="nav-item">
             <a className="nav-link active" aria-current="page" href="#">Home</a>
           </li>
